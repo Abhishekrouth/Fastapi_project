393237ff06a6c4846e610ec1d326b2f5b4a7d302
Abhishek Routh <routhabhishek6@gmail.com>
2025-09-24 17:26:57 +0530

    Created an endpoint to get all the avaialable books in library

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/books/{book_id}")
+async def available_book(book_id: int):
+    return {"book_id": book_id, "title": "Sample Book", "author": "Author Name"}
